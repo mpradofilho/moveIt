46bf9e40ac81e3e6e9f113ea9b7a63a3cdf325cf
Marcos Prado Filho <mpradofh@gmail.com>
2021-02-24 17:19:45 -0300

    Initial project

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,15 @@
+
+function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '60%' }} />
+        <span className="current-experience" style={{ left: '6mozi0%' }}>300xp</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+}
+
+export default ExperienceBar
